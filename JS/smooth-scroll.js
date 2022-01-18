42c1e8750311d876e62136c85194c616e86b34f4
useranbu <anbuarasan0344@gmail.com>
2022-01-18 14:35:57 +0530

    smoothScrollJS was not working

--- JS/smooth-scroll.js
+++ JS/smooth-scroll.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
       if (this.hash !== "") {
         // Prevent default anchor click behavior
         event.preventDefault();
-  
+        
         // Store hash
         var hash = this.hash;
-  
+
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
